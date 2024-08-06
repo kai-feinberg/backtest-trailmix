07931a3f8c28a4ac2e5c6b228dc91a8979426ad5
Kai Feinberg <kaiwfeinberg@gmail.com>
2024-08-06 10:50:41 -0500

    Initial commit

---
+++ data/config/colors.js
@@ -0,0 +1,19 @@
+/* Configure colors at https://shipixen.com/color-theme-explorer-shadcn */
+const colors = {
+  primary: {
+    lighter: "#7dd3fc",
+    light: "#38bdf8",
+    main: "#0ea5e9",
+    dark: "#0284c7",
+    darker: "#0369a1",
+  },
+  secondary: {
+    lighter: "#d1d5db",
+    light: "#9ca3af",
+    main: "#6b7280",
+    dark: "#4b5563",
+    darker: "#374151",
+  },
+};
+
+module.exports = { colors };
---
+++ src/App.tsx
@@ -0,0 +1,75 @@
+// import { useState } from 'react'
+import { LandingPrimaryImageCtaSection } from './components/landing/cta/LandingPrimaryCta'
+import { Button } from '@/components/shared/ui/button';
+import { LandingProductFeature } from './components/landing/LandingProductFeature';
+import { LandingProductFeatureKeyPoints } from './components/landing/LandingProductFeatureKeyPoints';
+import { Tweet } from 'react-tweet';
+import { LandingMarquee } from './components/landing/LandingMarquee';
+
+const keyPoints = [
+  {
+    title: 'Fast',
+    description:
+      'Capture screenshots in seconds. No more waiting. No more fiddling with scripts.',
+  },
+  {
+    title: 'Secure',
+    description:
+      '100% encrypted and secure. Your data is safe with us, always. Privacy first.',
+  },
+  {
+    title: 'Support',
+    description:
+      '24/7 customer support. We are here to help you at any time of the day. Just ask.',
+  },
+];
+
+function App() {
+  return (
+    <>
+      <div className='flex flex-col'>
+        <LandingPrimaryImageCtaSection
+          title="Stop roundtripping... please"
+          description="Get 10x more done with Shadcn UI, React & Next.js, and say goodbye to repetitive tasks. You'll never go back."
+          imageSrc='/ctaImage.png'
+          imageAlt='cta-image'
+          variant='primary'
+          withBackground={true}
+          withBackgroundGlow={true}
+          backgroundGlowVariant='primary'
+        >
+          <Button size="xl" asChild>
+            <a href="#" className='text-white'>Test Strategies</a>
+          </Button>
+        </LandingPrimaryImageCtaSection>
+{/*         
+        <div className="w-full overflow-hidden">
+          <LandingMarquee animationDirection='right' className='max-w-6xl'>
+            <Tweet id="1820298304028090650" />
+            <Tweet id="1820298304028090650" />
+            <Tweet id="1820298304028090650" />
+            <Tweet id="1820298304028090650" />
+          </LandingMarquee>
+        </div> */}
+
+
+        <LandingProductFeature
+          title="Crystal clear captures"
+          descriptionComponent={
+            <>
+              <LandingProductFeatureKeyPoints keyPoints={keyPoints} />
+            </>
+          }
+          imageSrc="/ctaImage.png"
+          imageAlt="Screenshot of the product"
+          imagePosition="left"
+          imagePerspective="right"
+        />
+      </div>
+
+
+    </>
+  )
+}
+
+export default App
